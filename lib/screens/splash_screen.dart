8fd9c7168ff86f4d819370252df077795d62d3a8
Briton Mhitike <101688136+Briton-james@users.noreply.github.com>
2024-05-07 16:49:33 +0300

    Added splash screen
    Added the splash screen at the start

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:school_connect/screens/welcome_screen.dart';
 import 'dart:async';
 
-import 'home_screen.dart'; // Import for Future.delayed
-
+import 'package:flutter/material.dart';
+import 'package:school_connect/screens/sign_in_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,54 +16,45 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Future.delayed(const Duration(seconds: 0), () {
       Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (context) => const HomeScreen()));
+          MaterialPageRoute(builder: (context) => const SignInScreen()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1061AD),
+      backgroundColor: const Color(0xFF0E424C),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/book.png',
-              height: 150.0,
-                width: 150.0,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            'assets/images/book.png',
+            height: 150.0,
+            width: 150.0,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          RichText(
+              text: const TextSpan(children: [
+            TextSpan(
+              text: 'School',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-
-              const SizedBox(
-                height: 20.0,
+            ),
+            TextSpan(children: [
+              TextSpan(
+                text: 'Connect',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
               ),
-
-              RichText(text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'School',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-
-                    TextSpan(
-                        children: [
-                          TextSpan(
-                            text: 'Connect',
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ]
-                    )
-                  ]
-              )
-              ),
-            ]
-        ),
+            ])
+          ])),
+        ]),
       ),
     );
   }
