3f5ac3890c8ec50dbf032cd25083df479fbecf40
Briton Mhitike <101688136+Briton-james@users.noreply.github.com>
2024-05-07 16:49:06 +0300

    Improved the signup
    reduced info and improve signup logic

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,183 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:school_connect/screens/home_screen.dart';
+import 'package:school_connect/components/widgets/school_signup.dart';
+import 'package:school_connect/components/widgets/volunteer_signup.dart';
 
-
-class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: const Color(0xFF1061AD),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-          child: ListView(
-            children: [
-              Image.asset('assets/images/book.png',
-                width: 100.0,
-                height: 100.0,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: const Color(0xFF0E424C),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF0E424C),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: 'As Volunteer',
               ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              Center(
-                child: RichText (text: const TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'School',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-
-                      TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Connect',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ]
-                      )
-                    ]
-                )
-               ),
-              ),
-
-              const Text('Create an account',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  //decoration: TextDecoration.underline,
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'First name',
-                  filled: true,
-                  fillColor: Colors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  )
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Surname',
-                  filled: true,
-                  fillColor: Colors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  )
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: 'email',
-                  filled: true,
-                  fillColor: Colors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular( 10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              TextField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: 'Phone number',
-                  filled: true,
-                  fillColor: Colors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular( 10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'password',
-                  filled: true,
-                  fillColor: Colors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular( 10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Confirm password',
-                  filled: true,
-                  fillColor: Colors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const HomeScreen()));
-                  //login logic
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
-                  maximumSize: const Size(150.0, 50.0),
-                ),
-                child: const Text('Sign up'),
+              Tab(
+                text: 'Register school',
               )
             ],
           ),
+        ),
+        body: const TabBarView(
+          children: [
+            VolunteerSignUpForm(),
+            SchoolSignUpForm(),
+          ],
         ),
       ),
     );
   }
 }
-
