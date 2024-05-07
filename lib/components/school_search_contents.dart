964ca97979ac99197e5f8ffbe20f272ebaed71fc
Briton Mhitike <101688136+Briton-james@users.noreply.github.com>
2024-05-07 16:39:35 +0300

    Search screen for school
    Search screen implemented

---
+++ lib/components/school_search_contents.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SchoolSearchContents extends StatelessWidget {
+  const SchoolSearchContents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        backgroundColor: const Color(0xff0E424C),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.grey,
+              prefixIcon: const Icon(Icons.search),
+              hintText: 'Search schools, subjects..',
+              hintFadeDuration: const Duration(seconds: 3),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
